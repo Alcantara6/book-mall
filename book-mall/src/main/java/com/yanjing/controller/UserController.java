40d21243fdd1e9d6918a0b80301b8adc87b19823
yanjing <yanjing@deepdraw.cn>
2021-02-23 15:13:01 +0800

    20. log4j和jacoco

--- book-mall/src/main/java/com/yanjing/controller/UserController.java
+++ book-mall/src/main/java/com/yanjing/controller/UserController.java
@@ -5,12 +5,15 @@ import com.yanjing.entity.User;
 import com.yanjing.service.UserService;
 import com.yanjing.util.response.Response;
 import com.yanjing.util.response.ResponseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
@@ -18,13 +21,16 @@ public class UserController {
     public Response<User> login(@RequestBody User user) {
         String requestUsername = user.getUsername();
         if (requestUsername == null) {
+            logger.error("用户名不能为空");
             return ResponseUtils.badRequest("用户名不能为空");
         }
         User registeredUser = userService.findByUsername(requestUsername).orElse(null);
         if (registeredUser == null) {
+            logger.error("用户不存在");
             return ResponseUtils.badRequest("用户不存在");
         }
         if (!registeredUser.getPassword().equals(user.getPassword())) {
+            logger.error("密码不正确，正确密码应为{}", registeredUser.getPassword());
             return ResponseUtils.badRequest("密码不正确");
         }
         userService.login(registeredUser);
