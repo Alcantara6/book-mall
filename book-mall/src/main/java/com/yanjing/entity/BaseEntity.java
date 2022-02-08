58445320490143c08f6e3e35dc2b2f4bcb03d486
yanjing <yanjing@deepdraw.cn>
2022-02-08 22:02:36 +0800

    @JsonIgnoreProperties

--- book-mall/src/main/java/com/yanjing/entity/BaseEntity.java
+++ book-mall/src/main/java/com/yanjing/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.yanjing.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
+@JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
